33ec71e6bf061316b8360b9837ca4920bbe82948
yildirim038 <aosmanyildirim@gmail.com>
2021-03-29 01:52:17 +0200

    ilk ödev

--- week4/Printing the Excel file with React/excel-with-file-react/src/components/excelTable.js
+++ week4/Printing the Excel file with React/excel-with-file-react/src/components/excelTable.js
@@ -3,28 +3,28 @@ import {readExcel,setData} from '../services/excelService.js';
 export  function ExcelToArray(){
     return(
     <div>
-    <input type="file" onChange={(e) => {
-          const file = e.target.files[0];
-          readExcel(file);
-        }}
-      />
-            <table class="table container">
+        <input type="file" onChange={(e) => {
+            const file = e.target.files[0];
+            readExcel(file);
+            }}
+        />
+        <table class="table container">
             <thead>
-            <tr>
-                <th>name</th>
-                <th>surname</th>
-                <th>alt</th>
-            </tr>
+                <tr>
+                    <th>name</th>
+                    <th>surname</th>
+                    <th>alt</th>
+                </tr>
             </thead>
             <tbody>
-            {setData.map((d) => (
-                d.map((e)=>
-                <tr key={e.name}>
-                <td>{e.name}</td>
-                <td>{e.surname}</td>
-                <td>{e.alt}</td>
-                </tr>
-            )))}
+                {setData.map((d) => (
+                    d.map((e)=>
+                    <tr key={e.name}>
+                    <td>{e.name}</td>
+                    <td>{e.surname}</td>
+                    <td>{e.alt}</td>
+                    </tr>
+                )))}
             </tbody>
         </table>
     </div>
