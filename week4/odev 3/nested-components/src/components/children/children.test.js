ab5218847322bca71f72fcf5ec866388e1b09038
yildirim038 <aosmanyildirim@gmail.com>
2021-04-12 10:04:08 +0200

    6.Hafta 1.Ödev

--- week4/odev 3/nested-components/src/components/children/children.test.js
+++ week4/odev 3/nested-components/src/components/children/children.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Childs from '../../App';
-
+/**
+ * Bu testler ile cocuk isim ve soy isminin ekrana gelip gelmedigini kontorol ediyoruz.
+ */
 test("Child First Name", async () => {
     render(<Childs />);
     const firstName = await screen.findByText('Hephzibah');
     expect(firstName).toBeInTheDocument();
   });
 
-  test("Child Last Name", async () => {
+ test("Child Last Name", async () => {
     render(<Childs/>);
     const lastName = await screen.findByText('Gervaise');
     expect(lastName).toBeInTheDocument();
--- week4/odev 3/nested-components/src/components/item/item.test.js
+++ week4/odev 3/nested-components/src/components/item/item.test.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Item from '../../App';
-
-
+/**
+ * Bu testler ile Kisinin isim ve soy isminin ekrana gelip gelmedigini kontorol ediyoruz.
+ */
 test("First Name", async () => {
     render(<Item/>);
     const firstName = await screen.findByText('Nesta');
     expect(firstName).toBeInTheDocument();
   });
 
-  test("Last Name", async () => {
+test("Last Name", async () => {
     render(<Item/>);
     const lastName = await screen.findByText('Farrance');
     expect(lastName).toBeInTheDocument();
