283719a21fd855ba8b828507f51f78e0a1305d15
yildirim038 <aosmanyildirim@gmail.com>
2021-04-12 10:03:40 +0200

    6.hafta 1. ödev

--- week4/odev 3/nested-components/src/components/friends/friends.test.js
+++ week4/odev 3/nested-components/src/components/friends/friends.test.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Friends from '../../App';
-
+/**
+ * Bu testler ile arkadas isim ve soy isminin ekrana gelip gelmedigini kontorol ediyoruz.
+ */
 test("Friend First Name", async () => {
     render(<Friends />);
     const firstName = await screen.findByText('Stevana');
     expect(firstName).toBeInTheDocument();
   });
 
-  test("Friend Last Name", async () => {
+test("Friend Last Name", async () => {
     render(<Friends />);
     const lastName = await screen.findByText('Giraldon');
     expect(lastName).toBeInTheDocument();
